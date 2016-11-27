4d6a0bdf6379dbaaa98ce82d4de28d2900ce0b8a
Will Hilton <wmhilton@gmail.com>
2016-11-27 14:51:50 -0500

    Implement saveAs

--- js-git-modern.js
+++ js-git-modern.js
@@ -1,15 +1,43 @@
-
-
-function GitRepo () {
-  let repo = {}
-  require('js-git/mixins/mem-db')(repo)
-  require('js-git/mixins/create-tree')(repo)
-  require('js-git/mixins/pack-ops')(repo)
-  require('js-git/mixins/walkers')(repo)
-  require('js-git/mixins/read-combiner')(repo)
-  require('js-git/mixins/formats')(repo)
-  return repo
+"use strict"
+function promisify (fn) {
+  return new Promise((resolve, reject) => {
+    fn((err, ...results) => {
+      return (err) ? reject(err) : resolve(...results)
+    })
+  })
 }
 
+
+const modes = require('js-git/lib/modes')
+
+class GitRepo {
+  constructor() {
+    let repo = {}
+    require('js-git/mixins/mem-db')(repo)
+
+    require('js-git/mixins/create-tree')(repo)
+    require('js-git/mixins/pack-ops')(repo)
+    require('js-git/mixins/walkers')(repo)
+    require('js-git/mixins/read-combiner')(repo)
+    require('js-git/mixins/formats')(repo)
+    this._repo = repo
+    // Object.defineProperty(this, '_repo', {
+    //   value: repo
+    // });
+    return this
+  }
+  saveAs (...args) { return promisify(this._repo.saveAs(...args)) }
+  loadAs (...args) { return promisify(this._repo.loadAs(...args)) }
+}
+
+  // repo.loadAs = loadAs;
+  // repo.saveRaw = saveRaw;
+  // repo.loadRaw = loadRaw;
+  // repo.hasHash = hasHash;
+  // repo.readRef = readRef;
+  // repo.updateRef = updateRef;
+  // repo.listRefs = listRefs;
+
 module.exports.GitRepo = GitRepo
+module.exports.GitModes = modes
 //module.exports.GitCommit = GitCommit
--- test/js-git-modern.test.js
+++ test/js-git-modern.test.js
@@ -1,7 +1,49 @@
 import test from 'ava'
 
-import { GitRepo } from '..'
+import { GitRepo, GitModes } from '..'
 
 test('exists', t => {
   t.truthy(GitRepo)
+  let repo = new GitRepo
+  t.is(typeof repo.saveAs, 'function')
+})
+
+test('create blob using original callback', async t => {
+  t.plan(1)
+  let repo = new GitRepo
+  repo._repo.saveAs("blob", "Hello World\n", (err, blobHash) => {
+    t.is(blobHash, "557db03de997c86a4a028e1ebd3a1ceb225be238")
+  })
+})
+
+test('create blob using new async/await interface', async t => {
+  t.plan(1)
+  let repo = new GitRepo
+  let blobHash = await repo.saveAs("blob", "Hello World\n")
+  t.is(blobHash, "557db03de997c86a4a028e1ebd3a1ceb225be238")
+})
+
+test('create a tree using original callback', async t => {
+  let repo = new GitRepo
+  t.plan(3)
+  repo._repo.saveAs("blob", "Hello World\n", (err, blobHash) => {
+    t.is(blobHash, "557db03de997c86a4a028e1ebd3a1ceb225be238")
+    repo._repo.saveAs("tree", {
+      "greeting.txt": { mode: GitModes.file, hash: blobHash }
+    }, (err, treeHash) => {
+      t.falsy(err)
+      t.is(treeHash, "648fc86e8557bdabbc2c828a19535f833727fa62")
+    })
+  })
+})
+
+test('create a tree using new async/await interface', async t => {
+  t.plan(2)
+  let repo = new GitRepo
+  let blobHash = await repo.saveAs("blob", "Hello World\n")
+  t.is(blobHash, "557db03de997c86a4a028e1ebd3a1ceb225be238")
+  let treeHash = await repo.saveAs("tree", {
+    "greeting.txt": { mode: GitModes.file, hash: blobHash }
+  })
+  t.is(treeHash, "648fc86e8557bdabbc2c828a19535f833727fa62")
 })
